987ec5aad858cb8c6b07767152709bcdc71ff7cf
Nikita <nikita.tiwari041999@gmail.com>
2023-09-08 18:03:01 +0530

    added cors access

--- api/index.js
+++ api/index.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 4000;
 
 
 app.use(express.json());
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 //-------------- jwt Middleware -------------------
 
 // Use the login and logout routes
