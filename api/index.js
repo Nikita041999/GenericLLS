0b065002bf8370e22081cbdfcc856c678460faf2
Nikita <nikita.tiwari041999@gmail.com>
2023-09-08 15:30:32 +0530

    test: added / url for testing of CI

--- api/index.js
+++ api/index.js
@@ -16,10 +16,11 @@ app.use(express.json());
 //-------------- jwt Middleware -------------------
 
 // Use the login and logout routes
+app.get('/',(req,res) => {
+  return res.send({mesage:"Get api successfully called."})
+})
 app.use('/api',router);
-// app.get('/',(req,res) => {
-//   return res.send({mesage:"Get api successfully called."})
-// })
+
 
 
 const startServer = async () => {
