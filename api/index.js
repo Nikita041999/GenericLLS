f65845fa90688d5c3fe73a877ed16d3ecb47189a
Nikita <nikita.tiwari041999@gmail.com>
2023-09-08 12:58:36 +0530

    endpoint updates

--- api/index.js
+++ api/index.js
@@ -6,22 +6,10 @@ const app = express();
 // const port = process.env.PORT || 3000;
 const port = process.env.PORT || 4000;
 
-// expressMailer.extend(app, {
-//   from: `${process.env.EMAIL_FROM}`,
-//   host: `${process.env.EMAIL_HOST}`, // Your SMTP server host
-//   secureConnection: false, // Use SSL
-//   port: 587, // Port for secure SMTP
-//   transportMethod: "SMTP",
-//   auth: {
-//     user: `${process.env.EMAIL_USER}`,
-//     pass: `${process.env.EMAIL_PASS}`,
-//   }
-// });
-
 
 app.use(express.json());
 // app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://generic-lls-obac.vercel.app');
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 //   next();
 // });
